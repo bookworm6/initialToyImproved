f33903d3527d19f0cc60aabc027b48fff56d734e
bookworm6 <bilborulesmordor@gmail.com>
2024-04-21 11:24:02 -0700

    camera now follows player when jumping up or down

--- project/main.gd
+++ project/main.gd
@@ -9,23 +9,13 @@ func _ready():
 	highestLocationReached = $Camera2D.get_global_position().y
 
 func _physics_process(delta):
-	if $player.get_global_position().y<highestLocationReached:
-		highestLocationReached = $player.get_global_position().y
-		$Camera2D.global_position.y = highestLocationReached
-	if $player.get_global_position().y>$Camera2D/FallingPoint.get_global_position().y:
-		falling = true
-		print("falling")
-	if falling==true:
-		$Camera2D.global_position.y = $player.get_global_position().y+$Camera2D/FallingPoint.get_position().y
+	$Camera2D.global_position.y = $player.get_global_position().y-$Camera2D/FallingPoint.get_position().y
+	
 		
 
 
 func _on_player_jumped():
 	numJumps+=1
-	$jumpLabel.text = "jumps: "+str(numJumps)
+	$CanvasLayer/Control/jumpLabel.text = "jumps: "+str(numJumps)
 
 
-func _on_player_landed():
-	falling = false
-	highestLocationReached = $Camera2D.get_global_position().y
-	
