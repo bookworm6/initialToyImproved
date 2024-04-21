ed2c598c87f55308901fa42b8209899dfa93e4e2
bookworm6 <bilborulesmordor@gmail.com>
2024-04-20 23:04:59 -0700

    camera follows player up

--- project/main.gd
+++ project/main.gd
@@ -1,9 +1,21 @@
 extends Node
 var numJumps
+var highestLocationReached
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	numJumps=0
+	highestLocationReached = $Camera2D.get_global_position().y
+
+func _physics_process(delta):
+	if $player.get_global_position().y<highestLocationReached:
+		print("camera scrolling")
+		highestLocationReached = $player.get_global_position().y
+		$Camera2D.global_position.y = highestLocationReached
+	if $player.get_global_position().y>$Camera2D/FallingPoint.get_global_position().y:
+		$Camera2D.global_position.y = $player.get_global_position().y
+		
+
 
 func _on_player_jumped():
 	numJumps+=1
